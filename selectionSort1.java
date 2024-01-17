66f7b277d0c0199eb9eda8626744a59d988e5dd1
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2024-01-18 02:29:07 +1100

    this is for p1

---
+++ selectionSort1.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+package dsa;
+
+public class selectionSort1 {
+    
+    public static void main(String args[])
+    {
+
+        // array
+        //this is for p1
+
+        int arr[] = {7,8,2,1,3}
+        // selection search
+        for (int i = 0 ; i< = )
+    }
+}
