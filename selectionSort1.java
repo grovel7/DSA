4e04e9743ad8c0472e9805767056f922126429a5
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2024-01-18 02:32:50 +1100

    p1 not so imp

--- selectionSort1.java
+++ selectionSort1.java
@@ -10,8 +10,15 @@ public class selectionSort1 {
         // array
         //this is for p1
 
+
         int arr[] = {7,8,2,1,3}
         // selection search
-        for (int i = 0 ; i< = )
+        for (int i = 0 ; i<  arr.length - 1;i++)
+        {
+            for ( int j =0;j<arr.length - 1;i++)
+            {
+                
+            }
+        }
     }
 }
